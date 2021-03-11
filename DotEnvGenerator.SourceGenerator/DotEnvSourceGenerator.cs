594e9281f7ba94695e4c88f1b15d167fc90a9db5
Mauricio <promomyl2@gmail.com>
2021-03-11 18:25:39 -0300

    Agregar archivos de proyecto.

---
+++ DotEnvGenerator.SourceGenerator/DotEnvSourceGenerator.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.CodeAnalysis;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DotEnvGenerator.SourceGenerator
+{
+    [Generator]
+    public class DotEnvSourceGenerator
+        : ISourceGenerator
+    {
+        public void Execute(GeneratorExecutionContext context)
+        {
+            
+            throw new NotImplementedException();
+        }
+
+        public void Initialize(GeneratorInitializationContext context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ SourceGenerator.Sample/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SourceGenerator.Sample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
